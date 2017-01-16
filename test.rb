dcff7284eb056557ad80f3e424c34b28fe9b88bc
vaxe <gma89i@gmail.com>
2017-01-16 13:36:04 +0700

    test_file_for

---
+++ test.rb
@@ -0,0 +1,32 @@
+class Test
+
+  def date
+    DateTime.now
+  end
+
+  def _gsub
+    url = 'http://example.com'
+    str = 'lisp-case-rules'
+
+    url.gsub('http://', 'https://')
+    str.gsub('-', '_')
+  end
+
+  def interpolation
+    "This is the #{result.to_s}."
+  end
+
+  def condition
+    if true then 1 else 2 end
+  end
+
+  def bang
+    ! false
+  end
+
+  def style
+      a = 1
+      a
+  end
+
+end
